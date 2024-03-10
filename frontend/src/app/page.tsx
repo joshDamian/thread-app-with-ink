4ed795ba00f0bf4ccc74ada440306d5e61113559
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2024-03-10 03:16:39 +0000

    feat: pass typedContract as props to ThreadsApp

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,21 @@
+import { ContractIds } from '@/deployments/deployments'
+import ThreadManagerContract from '@inkathon/contracts/typed-contracts/contracts/thread_manager'
+import { useRegisteredTypedContract } from '@scio-labs/use-inkathon'
+
 import ThreadsApp from '@/components/web3/thread/ThreadsApp'
 
 export default function HomePage() {
-  return <ThreadsApp />
+  const { typedContract } = useRegisteredTypedContract(
+    ContractIds.ThreadManager,
+    ThreadManagerContract,
+  )
+
+  if (!typedContract)
+    return (
+      <div className="flex h-[250px] w-full max-w-[620px] items-center justify-center rounded-md border border-dashed text-lg">
+        Connect your wallet to use Threads!
+      </div>
+    )
+
+  return <ThreadsApp threadManagerContract={typedContract} />
 }
--- frontend/src/components/web3/thread/ThreadsApp.tsx
+++ frontend/src/components/web3/thread/ThreadsApp.tsx
@@ -7,11 +7,7 @@ import { getAllThreads, getThreadInfo } from '@/data/adapters/browser/thread/get
 import { likeThread } from '@/data/adapters/browser/thread/update'
 import { ContractIds } from '@/deployments/deployments'
 import ThreadManagerContract from '@inkathon/contracts/typed-contracts/contracts/thread_manager'
-import {
-  useInkathon,
-  useRegisteredContract,
-  useRegisteredTypedContract,
-} from '@scio-labs/use-inkathon'
+import { useInkathon, useRegisteredContract } from '@scio-labs/use-inkathon'
 import { toast } from 'react-hot-toast'
 import Skeleton from 'react-loading-skeleton'
 import useSWR from 'swr'
@@ -19,38 +15,23 @@ import useSWR from 'swr'
 import ListThreads from '@/components/web3/thread/ListThreads'
 import ThreadMessageForm from '@/components/web3/thread/ThreadMessageForm'
 
-interface ThreadsAppProps {}
+interface ThreadsAppProps {
+  threadManagerContract: ThreadManagerContract
+}
 
-const ThreadsApp: FC<ThreadsAppProps> = () => {
+const ThreadsApp: FC<ThreadsAppProps> = ({ threadManagerContract }) => {
   const { api, activeAccount, activeSigner } = useInkathon()
   const { contract, address: contractAddress } = useRegisteredContract(ContractIds.ThreadManager)
 
-  const { typedContract } = useRegisteredTypedContract(
-    ContractIds.ThreadManager,
-    ThreadManagerContract,
-  )
-
   const { data: threadIds, error: threadsFetchError } = useSWR(
     'threads',
-    () =>
-      typedContract
-        ? getAllThreads({
-            typedContract: typedContract,
-          })
-        : undefined,
+    () => getAllThreads({ typedContract: threadManagerContract }),
     {
-      refreshInterval: 2000,
+      refreshInterval: 1000,
     },
   )
 
   if (threadsFetchError) return <div>Error Loading Threads</div>
-
-  if (!typedContract)
-    return (
-      <div className="flex h-[250px] w-full max-w-[620px] items-center justify-center rounded-md border border-dashed text-lg">
-        Connect your wallet to use Threads!
-      </div>
-    )
 
   if (!threadIds)
     return (
